a2c6d7083d7104c28d6c8e123bc0b6eec02c9624
Vivek Bhakta <vivek.bhakta@stridehealth.com>
2020-02-03 22:53:50 -0800

    Update script.js

--- relaxer-app/script.js
+++ relaxer-app/script.js
@@ -1,7 +1,7 @@
 const container = document.getElementById('container');
 const text = document.getElementById('text');
 
-const totalTime = 7500;
+const totalTime = 16000;
 const breatheTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
